8a430521b77eba452b8254ec285724248d59f193
Dino <86893313+dinorawraw@users.noreply.github.com>
2025-04-17 17:57:32 -0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,16 +1,16 @@
-import { initializeApp } from "firebase/app"
-import { getFirestore } from "firebase/firestore"
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCLy5LN6XUyylurZVRzzgBsh9oajk2Lj_4",
   authDomain: "supapp-86107.firebaseapp.com",
   databaseURL: "https://supapp-86107-default-rtdb.firebaseio.com",
   projectId: "supapp-86107",
-  storageBucket: "supapp-86107.firebasestorage.app",
+  storageBucket: "supapp-86107.appspot.com",
   messagingSenderId: "134578506306",
   appId: "1:134578506306:web:e02dfdd6a2a5d252287bdb",
-  measurementId: "G-KNWTEB2JEJ",
-}
+  measurementId: "G-KNWTEB2JEJ"
+};
 
-const app = initializeApp(firebaseConfig)
-export const db = getFirestore(app)
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
